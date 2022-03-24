196de2b6231f0196a01b9b96cd0352825f2f6469
Mathless <mrkalumba@gmail.com>
2022-03-24 23:14:24 +0300

    навёл красоту

--- Extracting_features.py
+++ Extracting_features.py
@@ -1,47 +1,56 @@
-import opensmile
+import os
+from glob import iglob
 
-# Выбрали параметры, которые хотим извлекать.
+import opensmile
 import pandas as pd
 
-smile = opensmile.Smile(
-    feature_set=opensmile.FeatureSet.emobase,
-    feature_level=opensmile.FeatureLevel.Functionals,
-)
+
+def get_feature_extractor():
+    # Выбираем извлекаемые параметры
+    return opensmile.Smile(
+        feature_set=opensmile.FeatureSet.emobase,  # Используемый набор фич.
+        feature_level=opensmile.FeatureLevel.Functionals,
+        # low-Level descriptors или Functionals (statistical, polynomial regression, and transformations). Подробнее:
+        # https://mediatum.ub.tum.de/doc/1082431/1082431.pdf
+    )
 
 
-# Выбираем аудиозапись
-def get_features(file_name: str):
+def get_features(file_name: str, feature_extractor):
+    """
+    На вход получает путь к файлу На выход выдаёт dataframe из одной строки Имя файла содержит в себе данные,
+    перечисленные через + (Пример: ./1070122155/1070122155+09_02_2022_19_32_43+покинутость_беспомощность_грусть+1.mp3
+    ) Эти данные добавляются в датафрэйм, полученный от обработки аудиофайла библиотекой opnesmile.
+    """
     user_id, date, emotions, text_number = file_name.split("/")[2].split("+")
-    y = smile.process_file(file_name)
+    y = feature_extractor.process_file(file_name)
     y.insert(0, "user_id", int(user_id))
-    y.insert(0, "date", date)
     y.insert(0, "emotions", emotions)
     y.insert(0, "text_number", text_number.split(".")[0])
     return y
 
 
-# compression_opts = dict(folder_name='out.csv')
+def extract_features_from_all_users_folders(root_dir: str):
+    """
+    Функция итерируется по папкам пользователей и запускает извлечение фич для каждой из найденных аудиозаписей.
+    :param root_dir: Путь до папки с базой данных. Пример: /home/tyoma/PycharmProjects/TelegramBot или если код из
+    папки с проектом rootdir = '.'
+    :return: Dataframe, с колонками: file, start, end, text_number, emotions, date,
+    user_id, ...(features).
+    """
+    root_dir += "/**/*"
+    file_list = [f for f in iglob(root_dir, recursive=True) if os.path.isfile(f)]
+    frames = []
+    fe = get_feature_extractor()
+    for file in file_list:
+        if file.endswith(".mp3"):
+            print(file)
+            y = get_features(str(file), feature_extractor=fe)
+            frames.append(y)
+    result = pd.concat(frames)
+
+    return result
 
 
-
-# Итерируемся по пользователям
-import os
-from glob import iglob
-
-rootdir_glob = './**/*'  # Note the added asterisks
-# This will return absolute paths
-file_list = [f for f in iglob(rootdir_glob, recursive=True) if os.path.isfile(f)]
-counter = 100
-frames = []
-for file in file_list:
-    if file.endswith(".mp3"):
-        print(file)
-        y = get_features(str(file))
-        frames.append(y)
-        counter -= 1
-        if counter == 0:
-            break
-result = pd.concat(frames)
-print(result)
-
-result.to_csv(path_or_buf="./csv_files/{}.csv".format("Emobase_extracted_features"), index=True)
+# Пример получения
+extract_features_from_all_users_folders(".").to_csv(
+    path_or_buf="./csv_files/{}.csv".format("Emobase_extracted_features"), index=True)
